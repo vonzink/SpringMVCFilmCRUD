ce52f3fe0a4c8f4253e0ab86c23bcdfb7179e400
Michel <michelmukua@Ryan-s-S20.localdomain>
2025-04-25 17:43:15 -0600

    create FilmDAO, add Actor Class

---
+++ src/main/java/com/skilldistillery/film/data/FilmDaoJdbcImpl.java
@@ -0,0 +1,46 @@
+package com.skilldistillery.film.data;
+
+import java.util.List;
+
+import com.skilldistillery.film.entities.Actor;
+import com.skilldistillery.film.entities.Film;
+
+public class FilmDaoJdbcImpl implements FilmDAO {
+
+	@Override
+	public Film findFilmById(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Actor findActorById(int actorId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Actor> findActorsByFilmId(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Film> findFilmByKeyword(String keyword) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String findFilmLanguage(int languageId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String AddFilm(String title) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}

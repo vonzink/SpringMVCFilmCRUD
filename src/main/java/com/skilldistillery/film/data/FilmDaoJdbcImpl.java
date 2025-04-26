0c3820b402ab8586b5890a16c0cf09a43be0b437
Vonzink <vonzink@gmail.com>
2025-04-25 18:19:10 -0600

    updated JSP file

--- src/main/java/com/skilldistillery/film/data/FilmDaoJdbcImpl.java
+++ src/main/java/com/skilldistillery/film/data/FilmDaoJdbcImpl.java
@@ -2,9 +2,12 @@ package com.skilldistillery.film.data;
 
 import java.util.List;
 
+import org.springframework.stereotype.Component;
+
 import com.skilldistillery.film.entities.Actor;
 import com.skilldistillery.film.entities.Film;
 
+@Component
 public class FilmDaoJdbcImpl implements FilmDAO {
 
 	@Override
